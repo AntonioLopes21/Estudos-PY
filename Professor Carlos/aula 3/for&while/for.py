7fee72023acbfa8c526895187f0200319f770266
Antonio.lopes <antonyejoao@gmail.com>
2024-10-15 01:58:32 -0300

    adicionando pastas para trabalhar amanhã

---
+++ Professor Carlos/aula 3/for&while/for.py
@@ -0,0 +1,14 @@
+#quando posso me considerar full stack?
+# trabalhar em todas as pontas
+# construir algo consolidado
+#dominar o crud
+# conseguir através de uma lang e framework construir um sistema web
+# para interação de user e api
+#restfull, autenticação e consumo de dados da api e do back no front
+
+
+#mvc -> divide pastas dos controllers
+#SPA
+
+# MVC É PARA QUEM ESTÁ COMEÇANDO A ESTUDAR?
+#  MVC E MVP
