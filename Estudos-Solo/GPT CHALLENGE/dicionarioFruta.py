8c16614baad6e0fb3115ee11376bbfda59ee2166
Antonio.lopes <antonyejoao@gmail.com>
2024-10-16 01:34:26 -0300

    adicionando próximo desafio

---
+++ Estudos-Solo/GPT CHALLENGE/dicionarioFruta.py
@@ -0,0 +1,9 @@
+frutas = {}
+
+fruta = input('digite o nome da fruta:')
+valor_uma_fruta = int(input('Digite o valor da fruta:'))
+quantidade_fruta = int(input('digite a quantidade da fruta:'))
+
+
+for i in quantidade_fruta:
+    
